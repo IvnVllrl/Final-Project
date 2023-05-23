64c66128bfc9fdcd3b8457a68311b7cfc19fed44
IvnVllrl <134377344+IvnVllrl@users.noreply.github.com>
2023-05-23 20:49:31 +0800

    Add files via upload

---
+++ final_project.sql
@@ -0,0 +1,100 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 23, 2023 at 02:48 PM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `final_project`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin_tbl`
+--
+
+CREATE TABLE `admin_tbl` (
+  `id` int(11) NOT NULL,
+  `username` varchar(150) NOT NULL,
+  `password` varchar(150) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `admin_tbl`
+--
+
+INSERT INTO `admin_tbl` (`id`, `username`, `password`) VALUES
+(1, 'admin', 'admin');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pwd_tbl`
+--
+
+CREATE TABLE `pwd_tbl` (
+  `id` int(11) NOT NULL,
+  `fname` varchar(150) NOT NULL,
+  `lname` varchar(150) NOT NULL,
+  `age` varchar(150) NOT NULL,
+  `pwdstate` varchar(150) NOT NULL,
+  `contactno` varchar(150) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `pwd_tbl`
+--
+
+INSERT INTO `pwd_tbl` (`id`, `fname`, `lname`, `age`, `pwdstate`, `contactno`) VALUES
+(1, 'Gen', 'Vill', '25', 'Blind', '09496431915');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admin_tbl`
+--
+ALTER TABLE `admin_tbl`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `pwd_tbl`
+--
+ALTER TABLE `pwd_tbl`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `admin_tbl`
+--
+ALTER TABLE `admin_tbl`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `pwd_tbl`
+--
+ALTER TABLE `pwd_tbl`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
